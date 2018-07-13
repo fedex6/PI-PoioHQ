f79275592e2ea78c9019a2c09b98fd0c917b689a
Fedex <fedex6@gmail.com>
2018-07-13 14:25:29 -0300

    Documentacion

--- PoioHQ_bot.py
+++ PoioHQ_bot.py
@@ -25,29 +25,42 @@ def handle(msg):
 
     #if chat_id == chat_id from owner :
     if 1:
-        if command =='/roll':
+        if command =='/roll': ## Es solo de prueba, para ver si funciona el bot
             bot.sendMessage(chat_id, random.randint(1, 6))
 
-        if command == '/id':
+        if command == '/id': ## Devuelve el numero de id del chat, para luego configurar para que solo lo use el dueño [probar otros metodos]
             bot.sendMessage(chat_id, chat_id)
 
-        if command == '/iniciar':
+        if command == '/iniciar': ## Inicia la deteccion de movimiento
             os.system('python pys/pir.py &')
-            bot.sendMessage(chat_id,'Iniciada la deteccion de los PIRs')
-            print('PIR: Start at ' + time.ctime() + '\n')
+            bot.sendMessage(chat_id,'Iniciada la deteccion de los PIRs') ## Envia un mensaje por Telegram
+            ## LOG - deja registro del inicio
+            log = open("log.txt", "w")
+            log.write('PIR: Start at ' + time.ctime() + '\n')
+            log.close()
             
-        if command == '/stop':
-            os.system('pkill -9 -f pir.py &')
-            bot.sendMessage(chat_id,'Detenida la deteccion de los PIRs')
-            print('PIR: Stop at ' + time.ctime() + '\n')
+        if command == '/stop': ## Para el py de la deteccion de movimiento
+            os.system('pkill -9 -f pir.py &') ## este comando hay que revisarlo
+            bot.sendMessage(chat_id,'Detenida la deteccion de los PIRs') ## Envia un mensaje por Telegram
+            ## LOG - deja registro del "apagado" de la deteccion de movimiento
+            log = open("log.txt", "w")
+            log.write('PIR: Stop at ' + time.ctime() + '\n')
+            log.close()
 
-    else:
-        bot.sendMessage(chat_id,'Sal de aqui maldito bastardo !')
-        print(chat_id + ' intento usar el bot. ( ' + time.ctime() + ' )\n' )
+    else: ## Cuando se activa el bloqueo para que solo lo use el dueño y lo intenta usar otra persona
+        bot.sendMessage(chat_id,'Sal de aqui maldito bastardo !') ## Envia un mensaje por Telegram
+        ## LOG - deja registro
+        log = open("log.txt", "w")
+        log.write(chat_id + ' intento usar el bot. ( ' + time.ctime() + ' )\n' )
+        log.close()
 
-bot = telepot.Bot('--TOKEN--')
+bot = telepot.Bot('--TOKEN--') ## Poner el TOKEN del bot
 bot.message_loop(handle)
-print('Iniciando...')
+
+##LOG - Deja registro de que se inicio el programa
+log = open("log.txt", "w")
+log.write('Iniciando... [ ' + time.ctime() + ' ]\n')
+log.close()
 
 while 1:
     try:
