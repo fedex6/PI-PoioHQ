f594bda399f0bccfb8058d7ded1a29e7599c4204
Fedex <fedex6@gmail.com>
2018-07-18 11:29:22 -0300

    Tabs a espacios
    Tucumetal rompe tarlipes

--- pys/pir.py
+++ pys/pir.py
@@ -19,14 +19,14 @@ bot = telepot.Bot('--TOKEN--')
 
 while 1:
     try:
-		if GPIO.input(18):
-			bot.sendMessage('chat_id from owner',"Se detecto movimiento en (1) " + time.ctime())
-			## LOG
-	        log = open("../log.txt", "a")
-	        log.write('[ ' + time.ctime() + ' ] >>> Movimiento detectado en (1)\n')
-	        log.close()
-			time.sleep(2.9)
-		time.sleep(0.1)
+        if GPIO.input(18):
+        bot.sendMessage('chat_id from owner',"Se detecto movimiento en (1) " + time.ctime())
+        ## LOG
+        log = open("../log.txt", "a")
+        log.write('[ ' + time.ctime() + ' ] >>> Movimiento detectado en (1)\n')
+        log.close()
+        time.sleep(2.9)
+        time.sleep(0.1)
 
     except KeyboardInterrupt:
         print('\n Program interrupted')
